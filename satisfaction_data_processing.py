2559f8662a9be014ab5f90e9e40147af5ce4b432
vdabro2 <vdabro2@illinois.edu>
2024-04-16 12:15:37 -0500

    Cleaning csv data

--- satisfaction_data_processing.py
+++ satisfaction_data_processing.py
@@ -1,1 +1,5 @@
 # todo: process all data and create a dataset for average satisfaction rates
+import pandas as pd
+for i in range(1,9):
+    data = pd.read_csv('transcripts/sentiment_data' + str(i) +'.csv')
+    
