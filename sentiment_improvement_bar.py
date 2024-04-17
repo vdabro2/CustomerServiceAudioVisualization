2accc63cce3750de7a29ed8ad0d1cf5ea577066a
vdabro2 <vdabro2@illinois.edu>
2024-04-16 19:38:39 -0500

    Customer satisfaction visual

--- sentiment_improvement_bar.py
+++ sentiment_improvement_bar.py
@@ -2,13 +2,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-improvement_data = pd.read_csv('satisfaction_data/all.csv')
+improvement_data = pd.read_csv('satisfaction_data/improvement_rates.csv')
+improvement_data['Improvement_Rate'] = improvement_data['Improvement_Rate']* 100
 dates = ['2024-04-04', '2024-04-05', '2024-04-06', '2024-04-07', '2024-04-08', '2024-04-09']
 average_improvement_rate = improvement_data.groupby('Date')['Improvement_Rate'].mean()
 print(average_improvement_rate)
-average_improvement_rate.plot.bar(x=average_improvement_rate.index, y='Improvement_Rate', color='skyblue')
+average_improvement_rate.plot.bar(x=average_improvement_rate.index, y='Improvement_Rate', color='#c3a4cf')
 plt.xlabel('Date')
-plt.ylabel('Improvement Rate')
+plt.ylabel('Improvement Rate (%)')
 plt.title('Customer Mood Improvement Averages')
 plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
 plt.grid(True)
