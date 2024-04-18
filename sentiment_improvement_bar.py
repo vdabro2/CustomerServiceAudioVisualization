8f5ce6f4d68791f34925afcf32a5a790142aaf87
Grace Jia <gracejj2@illinois.edu>
2024-04-18 15:14:24 -0500

    add tooltip for bar chart

--- sentiment_improvement_bar.py
+++ sentiment_improvement_bar.py
@@ -1,6 +1,7 @@
 # create the bar chart for average sentiment improvement for customers on a given day
 import pandas as pd
 import matplotlib.pyplot as plt
+import mplcursors
 
 improvement_data = pd.read_csv('satisfaction_data/improvement_rates.csv')
 improvement_data['Improvement_Rate'] = improvement_data['Improvement_Rate']* 100
@@ -14,4 +15,15 @@ plt.title('Average Customer Mood Improvement per Day')
 plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
 plt.grid(True)
 plt.tight_layout()  # Adjust layout to prevent clipping of labels
+
+cursor = mplcursors.cursor(hover=mplcursors.HoverMode.Transient)
+@cursor.connect("add")
+def on_add(sel):
+    x, y, width, height = sel.artist[sel.index].get_bbox().bounds
+    sel.annotation.get_bbox_patch().set(fc="white")
+    sel.annotation.arrow_patch.set(arrowstyle="simple", fc="white", alpha=.8)
+    sel.annotation.set(text=f"{round(height, 2)}",
+                       position=(0, 20), anncoords="offset points")
+    sel.annotation.xy = (x + width / 2, y + height)
+
 plt.show()
