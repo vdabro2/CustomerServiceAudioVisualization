e1fb858b224c7a8c5ba8336d7a0493a73c6ac8b8
Grace Jia <gracejj2@illinois.edu>
2024-04-18 10:25:46 -0500

    Add tooltip for sentiment avg plot

--- sentiment_avg_plot.py
+++ sentiment_avg_plot.py
@@ -5,30 +5,87 @@ import mplcursors
 
 improvement_data = pd.read_csv('satisfaction_data/avg_sentiments.csv')
 dates = ['2024-04-04', '2024-04-05', '2024-04-06', '2024-04-07', '2024-04-08', '2024-04-09']
-Negative_Customer = improvement_data.groupby('Date')['Negative_Customer'].mean()
-Neutral_Customer = improvement_data.groupby('Date')['Neutral_Customer'].mean()
-Positive_Customer = improvement_data.groupby('Date')['Positive_Customer'].mean()
-Negative_Agent = improvement_data.groupby('Date')['Negative_Agent'].mean()
-Neutral_Agent = improvement_data.groupby('Date')['Neutral_Agent'].mean()
-Positive_Agent = improvement_data.groupby('Date')['Positive_Agent'].mean()
+Negative_Customer = improvement_data.groupby('Date')['Negative_Customer'].mean()*100
+Neutral_Customer = improvement_data.groupby('Date')['Neutral_Customer'].mean()*100
+Positive_Customer = improvement_data.groupby('Date')['Positive_Customer'].mean()*100
+Negative_Agent = improvement_data.groupby('Date')['Negative_Agent'].mean()*100
+Neutral_Agent = improvement_data.groupby('Date')['Neutral_Agent'].mean()*100
+Positive_Agent = improvement_data.groupby('Date')['Positive_Agent'].mean()*100
 fig, ax = plt.subplots(figsize=(10, 6))
 
 
 plt.xlabel('Date')
-plt.plot(Negative_Customer, label='Negative Customer', linestyle='dotted', marker='o', color='#bf2b23')
-plt.plot(Neutral_Customer, label='Neutral Customer', linestyle='dotted', marker='o', color='#c3a4cf')
-plt.plot(Positive_Customer, label='Positive Customer', linestyle='dotted', marker='o', color='#2f67b1')
-plt.plot(Negative_Agent, label='Negative Agent', linestyle='-', marker='o', color='#bf2b23')
-plt.plot(Neutral_Agent, label='Neutral Agent', linestyle='-', marker='o', color='#c3a4cf')
-plt.plot(Positive_Agent, label='Positive Agent', linestyle='-', marker='o', color='#2f67b1')
+neg_customer, = plt.plot(Negative_Customer, label='Negative Customer', linestyle='dotted', marker='o', color='#bf2b23')
+neu_customer, = plt.plot(Neutral_Customer, label='Neutral Customer', linestyle='dotted', marker='o', color='#c3a4cf')
+pos_customer, = plt.plot(Positive_Customer, label='Positive Customer', linestyle='dotted', marker='o', color='#2f67b1')
+neg_agent, = plt.plot(Negative_Agent, label='Negative Agent', linestyle='-', marker='o', color='#bf2b23')
+neu_agent, = plt.plot(Neutral_Agent, label='Neutral Agent', linestyle='-', marker='o', color='#c3a4cf')
+pos_agent, = plt.plot(Positive_Agent, label='Positive Agent', linestyle='-', marker='o', color='#2f67b1')
 
 
-plt.ylabel('Percentage')
+plt.ylabel('Percentage (%)')
 plt.title('Average Sentiments per Day')
 plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
 plt.grid(True)
 plt.legend(loc=2)
-# todo add tooltips
+
+# Tooltips
+annot = ax.annotate("", xy=(0,0), xytext=(-20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+annot.set_visible(False)
+
+def update_annot(ind, data):
+    x,y = data.get_data()
+    annot.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
+    text = "{}".format(str(round(y[ind["ind"][0]], 2)))
+    annot.set_text(text)
+    annot.get_bbox_patch().set_alpha(0.4)
+
+def hover(event):
+    vis = annot.get_visible()
+    if event.inaxes == ax:
+        cont, ind, data = find_data_point(event)
+
+        if cont:
+            update_annot(ind, data)
+            annot.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annot.set_visible(False)
+                fig.canvas.draw_idle()
+
+def find_data_point(event):
+    cont, ind = neg_customer.contains(event)
+    if cont:
+        return (cont, ind, neg_customer)
+    
+    cont, ind = neu_customer.contains(event)
+    if cont:
+        return (cont, ind, neu_customer)
+    
+    cont, ind = pos_customer.contains(event)
+    if cont:
+        return (cont, ind, pos_customer)
+    
+    cont, ind = neg_agent.contains(event)
+    if cont:
+        return (cont, ind, neg_agent)
+    
+    cont, ind = neu_agent.contains(event)
+    if cont:
+        return (cont, ind, neu_agent)
+    
+    cont, ind = pos_agent.contains(event)
+    if cont:
+        return (cont, ind, pos_agent)
+    
+    else:
+        return (False, None, None)
+
+fig.canvas.mpl_connect("motion_notify_event", hover)
 
 plt.tight_layout()  # Adjust layout to prevent clipping of labels
+
 plt.show()
